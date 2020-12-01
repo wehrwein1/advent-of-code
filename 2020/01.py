a4a3060d93c667059283850848f3b6454f2544f4
wehrwein1 <23436190+wehrwein1@users.noreply.github.com>
2020-12-01 00:23:10 -0500

    2020 day 1

--- 2020/01.py
+++ 2020/01.py
@@ -1,1 +1,11 @@
-
+# https://adventofcode.com/2020/day/1
+# Part 1: 2SUM
+# Part 2: 3SUM
+entries = list(map(int, [line for line in map(str.rstrip, open('input/01_INPUT.txt'))]))
+print(entries)
+for i in range(len(entries)):
+  for j in range(len(entries)):
+    for k in range(len(entries)):
+      if entries[i] + entries[j] + entries[k] == 2020:
+        print("mult=", entries[i] * entries[j] * entries[k])
+        exit()
