6e056b642c4978330f1b8dd15b829280c074568c
wehrwein1 <23436190+wehrwein1@users.noreply.github.com>
2020-12-02 08:57:42 -0500

    fix formatting

--- 2020/02.py
+++ 2020/02.py
@@ -5,17 +5,13 @@ lines = [line for line in map(str.rstrip, open('input/02_INPUT.txt'))]
 # lines = ['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']
 print('input (len={}):'.format(lines, len(lines)))
 
-def is_valid_password_part1(password : str, 
-                            policy_range : List[str],
-                            policy_char : str):
+def is_valid_password_part1(password : str, policy_range : List[int], policy_char : str):
   char_count = sum([1 for ch in password if ch == policy_char])
   min_allowed, max_allowed = policy_range
   return char_count >= min_allowed and char_count <= max_allowed
   min_allowed, max_allowed = map(int, char_range.split('-'))
 
-def is_valid_password_part2(password : str,
-                            policy_range : List[str],
-                            policy_char : str):
+def is_valid_password_part2(password : str, policy_range : List[int], policy_char : str):
   first, second = policy_range
   return (password[first-1] == policy_char) ^ (password[second-1] == policy_char)
 
