854090a5eeb3c43fd30d11e587277e8d763da742
wehrwein1 <23436190+wehrwein1@users.noreply.github.com>
2021-12-06 17:08:17 -0500

    day 6 part 2

--- 2021/day06/06.go
+++ 2021/day06/06.go
@@ -10,7 +10,10 @@ import (
 func main() {
 	initialAges := util.FileContent("../input/06_INPUT.txt")
 	println(fmt.Sprintf("part 1: lanternfish count (80 days) %d", len(simulateLanternfish(initialAges, 80))))
+	println(fmt.Sprintf("part 2: lanternfish count (256 days) %d", simulateLanternfishCount(initialAges, 256)))
 }
+
+var sum = util.SumInts
 
 func simulateLanternfish(agesCsv string, daysCount int) []int {
 	ages := util.StringSplitToInts(agesCsv, ",")
@@ -28,7 +31,37 @@ func simulateLanternfish(agesCsv string, daysCount int) []int {
 		for c := 0; c < appendCount; c++ {
 			ages = append(ages, 8)
 		}
+		// println(fmt.Sprintf("iteration %2d, len(ages)=%d incl added=%d", (i + 1), len(ages), appendCount))
 		// println(fmt.Sprintf("After %3d days: %v", (i + 1), ages))
 	}
 	return ages
+}
+
+// part 2 - vastly improved memory usage
+func simulateLanternfishCount(agesCsv string, daysCount int) int {
+	// populate counts from input
+	ageCounts := make([]int, 9)
+	for i := range ageCounts {
+		ageCounts[i] = 0
+	}
+	for _, age := range util.StringSplitToInts(agesCsv, ",") {
+		ageCounts[age] += 1
+	}
+	// iterate days
+	for i := 0; i < daysCount; i++ {
+		newSixesCount := ageCounts[0]   // all 0->6
+		newEightsCount := newSixesCount // +1 8 for each 0->6
+		// shuffle down
+		ageCounts[0] = ageCounts[1]
+		ageCounts[1] = ageCounts[2]
+		ageCounts[2] = ageCounts[3]
+		ageCounts[3] = ageCounts[4]
+		ageCounts[4] = ageCounts[5]
+		ageCounts[5] = ageCounts[6]
+		ageCounts[6] = ageCounts[7] + newSixesCount
+		ageCounts[7] = ageCounts[8]
+		ageCounts[8] = newEightsCount
+		println(fmt.Sprintf("iteration %4d ageCounts= %v", i+1, ageCounts))
+	}
+	return sum(ageCounts...)
 }
--- 2021/day06/06_test.go
+++ 2021/day06/06_test.go
@@ -6,10 +6,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDay(t *testing.T) {
+func TestDayPart1(t *testing.T) {
 	testcase := "3,4,3,1,2"
 	assert.Equal(t, 26, len(simulateLanternfish(testcase, 18)))
 	assert.Equal(t, []int{6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8}, simulateLanternfish(testcase, 18))
 	assert.Equal(t, 5934, len(simulateLanternfish(testcase, 80)))
-	// assert.Equal(t, 26984457539, len(simulateLanternfish(testcase, 256)))
+}
+
+func TestDayPart2(t *testing.T) {
+	testcase := "3,4,3,1,2"
+	assert.Equal(t, 26, simulateLanternfishCount(testcase, 18))
+	assert.Equal(t, 5934, simulateLanternfishCount(testcase, 80))
+	assert.Equal(t, 26984457539, simulateLanternfishCount(testcase, 256))
 }
