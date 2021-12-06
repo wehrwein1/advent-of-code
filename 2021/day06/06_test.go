eafb3ea07ec97b2101c2383943b6b4e661641a4d
wehrwein1 <23436190+wehrwein1@users.noreply.github.com>
2021-12-06 15:38:06 -0500

    day 6 part 1

--- 2021/day06/06_test.go
+++ 2021/day06/06_test.go
@@ -7,7 +7,9 @@ import (
 )
 
 func TestDay(t *testing.T) {
-	testcase := parseAges("3,4,3,1,2")
-	assert.Equal(t, 26, simulateLanternfish(testcase, 18))
-	assert.Equal(t, 5934, simulateLanternfish(testcase, 80))
+	testcase := "3,4,3,1,2"
+	assert.Equal(t, 26, len(simulateLanternfish(testcase, 18)))
+	assert.Equal(t, []int{6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8}, simulateLanternfish(testcase, 18))
+	assert.Equal(t, 5934, len(simulateLanternfish(testcase, 80)))
+	// assert.Equal(t, 26984457539, len(simulateLanternfish(testcase, 256)))
 }
