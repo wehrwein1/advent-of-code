01e61b7937cf8e661592a5231a8e76986d319b34
wehrwein1 <23436190+wehrwein1@users.noreply.github.com>
2021-12-10 16:05:04 -0500

    find 2d neighbors + tests

--- util/ints.go
+++ util/ints.go
@@ -87,3 +87,22 @@ func Int2dArrayHasValueAtPos(rowsAndCols [][]int, rowIndex int, colIndex int) (f
 	}
 	return 0 /* arbitrary not found value*/, false
 }
+
+func Int2dArrayFindNeighbors(rowsAndCols [][]int, rowIndex int, colIndex int) (neighbors []Int2dArrayNeighbor) {
+	directions := []Direction{North, East, South, West}
+	for _, direction := range directions {
+		row, col := direction.Translate(rowIndex, colIndex)
+		value, isFound := Int2dArrayHasValueAtPos(rowsAndCols, row, col)
+		if isFound {
+			neighbors = append(neighbors, Int2dArrayNeighbor{Row: row, Col: col, Value: value, Direction: direction})
+		}
+	}
+	return
+}
+
+type Int2dArrayNeighbor struct {
+	Row       int
+	Col       int
+	Value     int       // coordinate position value
+	Direction Direction // coordinate position relative to external origin
+}
