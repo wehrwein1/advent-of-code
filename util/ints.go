2b00b3dcd6d9cc4fb4d90c4bad76742072ea890c
wehrwein1 <23436190+wehrwein1@users.noreply.github.com>
2021-12-10 16:56:22 -0500

    refactor 2d int arrays out from ints

--- util/ints.go
+++ util/ints.go
@@ -76,33 +76,3 @@ func MapIntsToBools(items []int, f func(int) bool) []bool {
 	}
 	return boolValues
 }
-
-func Int2dArrayHasValueAtPos(rowsAndCols [][]int, rowIndex int, colIndex int) (foundValue int, isFound bool) {
-	rowCount := len(rowsAndCols)
-	colCount := len(rowsAndCols[0])
-	rowOk := (0 <= rowIndex) && (rowIndex < rowCount)
-	colOk := (0 <= colIndex) && (colIndex < colCount)
-	if rowOk && colOk {
-		return rowsAndCols[rowIndex][colIndex], true
-	}
-	return 0 /* arbitrary not found value*/, false
-}
-
-func Int2dArrayFindNeighbors(rowsAndCols [][]int, rowIndex int, colIndex int) (neighbors []Int2dArrayNeighbor) {
-	directions := []Direction{North, East, South, West}
-	for _, direction := range directions {
-		row, col := direction.Translate(rowIndex, colIndex)
-		value, isFound := Int2dArrayHasValueAtPos(rowsAndCols, row, col)
-		if isFound {
-			neighbors = append(neighbors, Int2dArrayNeighbor{Row: row, Col: col, Value: value, Direction: direction})
-		}
-	}
-	return
-}
-
-type Int2dArrayNeighbor struct {
-	Row       int
-	Col       int
-	Value     int       // coordinate position value
-	Direction Direction // coordinate position relative to external origin
-}
--- util/ints_test.go
+++ util/ints_test.go
@@ -47,35 +47,3 @@ func TestMapIntsToBools(t *testing.T) {
 func TestSortedInts(t *testing.T) {
 	assert.Equal(t, []int{1, 2, 3, 4, 5}, SortedInts(4, 1, 2, 5, 3))
 }
-
-func TestInt2dArrayHasValueAtPos(t *testing.T) {
-	grid := [][]int{{0, 1, 2, 3},
-		{4, 5, 6, 7},
-		{8, 9, 10, 11}}
-	{
-		val, isFound := Int2dArrayHasValueAtPos(grid, 0, 0)
-		assert.Equal(t, 0, val)
-		assert.Equal(t, true, isFound)
-	}
-	{
-		_, isFound := Int2dArrayHasValueAtPos(grid, 5, 5)
-		assert.Equal(t, false, isFound)
-	}
-	{
-		_, isFound := Int2dArrayHasValueAtPos(grid, -1, 8)
-		assert.Equal(t, false, isFound)
-	}
-}
-
-func TestInt2dArrayFindNeighbors(t *testing.T) {
-	grid := [][]int{{0, 1, 2, 3},
-		{4, 5, 6, 7},
-		{8, 9, 10, 11}}
-	assert.Equal(t, 5, grid[1][1])
-	assert.Equal(t, []Int2dArrayNeighbor{
-		{Row: 0, Col: 1, Value: 1, Direction: North}, // north of 5
-		{Row: 1, Col: 2, Value: 6, Direction: East},  // east of 5
-		{Row: 2, Col: 1, Value: 9, Direction: South}, // south of 5
-		{Row: 1, Col: 0, Value: 4, Direction: West},  // west of 5
-	}, Int2dArrayFindNeighbors(grid, 1, 1))
-}
