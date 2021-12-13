087b82c626c335deaa791bd51fa6cfa1a85e56a9
wehrwein1 <23436190+wehrwein1@users.noreply.github.com>
2021-12-13 15:58:02 -0500

    day 11 WIP

--- 2021/day11/11.go
+++ 2021/day11/11.go
@@ -7,43 +7,54 @@ import (
 	"github.com/wehrwein1/advent-of-code/util"
 )
 
-var sum = util.SumInts
-var product = util.ProductInts
-var all = util.AllTrue
+// var sum = util.SumInts
+// var product = util.ProductInts
+// var all = util.AllTrue
+type If = util.If
+
 var fileLines = util.FileLinesSkipEmpty
 var AllDirections = util.AllDirections
 var parseIntGrid = util.StringsToInt2dArray
+var findZeroCellValue = func(g [][]int, cellValue int, p util.Point) bool { return cellValue == 0 }
+var incrementCellValue = func(g [][]int, cellValue int, point util.Point) int { return If(cellValue < 9).Int(cellValue+1, 0) }
 
 func main() {
-	println(fmt.Sprintf("part 1: count of flashes %d", countFlashes(parseIntGrid(fileLines("../input/11_INPUT.txt")), 100)))
+	println(fmt.Sprintf("part 1: count of flashes %d", simulate(parseIntGrid(fileLines("../input/11_INPUT.txt")), 100)))
 }
 
-func countFlashes(grid [][]int, simulationCount int) (flashCount int) {
+func simulate(grid [][]int, simulationCount int) (flashCount int) {
+	for i := 0; i < simulationCount; i++ {
+		util.Int2dArrayMap(grid, incrementCellValue)                         // += 1 everywhere
+		primaryFlashes := util.Int2dArrayFindPoints(grid, findZeroCellValue) // find primary flashes
+		flashCount += len(primaryFlashes)
 
-	return
-}
+		// TODO FIXME cascades
 
-func findLowPoints(digitRows [][]int) (lowPoints []LowPoint) {
-	rowCount := len(digitRows)
-	colCount := len(digitRows[0])
-	println(fmt.Sprintf("rowCount %d, colCount %d", rowCount, colCount))
-	for r := 0; r < rowCount; r++ {
-		for c := 0; c < colCount; c++ {
-			currentValue := digitRows[r][c]
-			// println(fmt.Sprintf("row %d col %d => %d", r, c, currentValue))
-			neighbors := util.Int2dArrayFindNeighbors(digitRows, r, c, AllDirections)
-			neighborValues := []int{}
-			for _, neighbor := range neighbors {
-				neighborValues = append(neighborValues, neighbor.Value)
-			}
-			isLowPoint := all(util.MapIntsToBools(neighborValues, func(value int) bool { return currentValue < value })...)
-			if isLowPoint {
-				lowPoints = append(lowPoints, LowPoint{Row: r, Col: c, Value: currentValue})
-			}
-		}
 	}
 	return
 }
+
+// func findLowPoints(digitRows [][]int) (lowPoints []LowPoint) {
+// 	rowCount := util.RowCount(digitRows)
+// 	colCount := util.ColCount(digitRows)
+// 	println(fmt.Sprintf("rowCount %d, colCount %d", rowCount, colCount))
+// 	for r := 0; r < rowCount; r++ {
+// 		for c := 0; c < colCount; c++ {
+// 			currentValue := digitRows[r][c]
+// 			// println(fmt.Sprintf("row %d col %d => %d", r, c, currentValue))
+// 			neighbors := util.Int2dArrayFindNeighbors(digitRows, r, c, AllDirections)
+// 			neighborValues := []int{}
+// 			for _, neighbor := range neighbors {
+// 				neighborValues = append(neighborValues, neighbor.Value)
+// 			}
+// 			isLowPoint := all(util.MapIntsToBools(neighborValues, func(value int) bool { return currentValue < value })...)
+// 			if isLowPoint {
+// 				lowPoints = append(lowPoints, LowPoint{Row: r, Col: c, Value: currentValue})
+// 			}
+// 		}
+// 	}
+// 	return
+// }
 
 var isGridPointValueLessThan9 = func(grid [][]int, p util.Point) bool { return grid[p.X][p.Y] < 9 }
 
