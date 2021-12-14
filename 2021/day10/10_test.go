cdf78d14b3917055db6dde6c8e70efee279e7dd1
wehrwein1 <23436190+wehrwein1@users.noreply.github.com>
2021-12-13 19:20:57 -0500

    day 10 WIP

--- 2021/day10/10_test.go
+++ 2021/day10/10_test.go
@@ -6,6 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDay(t *testing.T) {
+func SkipTestDay(t *testing.T) {
 	assert.Equal(t, -1, computeDay(fileLines("../input/10_TEST.txt")))
 }
