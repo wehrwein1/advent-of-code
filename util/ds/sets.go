608f95d1a18f30a560822a5f7ea86393417d976d
wehrwein1 <23436190+wehrwein1@users.noreply.github.com>
2021-12-16 08:55:56 -0500

    RuneSet()

--- util/ds/sets.go
+++ util/ds/sets.go
@@ -1,5 +1,7 @@
 package ds
 
+// TODO FIXME this likely goes away once generics arrives
+
 type IntSet struct { // would generalize further if had generics
 	data map[int]bool
 }
@@ -13,6 +15,25 @@ func (s IntSet) Put(val int) {
 }
 
 func (s IntSet) Has(val int) bool {
+	_, ok := s.data[val]
+	return ok
+}
+
+type RuneSet struct { // would generalize further if had generics
+	data map[rune]bool
+}
+
+func NewRuneSet() *RuneSet {
+	return &RuneSet{data: make(map[rune]bool)}
+}
+
+func (s RuneSet) Put(vals ...rune) {
+	for _, val := range vals {
+		s.data[val] = true
+	}
+}
+
+func (s RuneSet) Has(val rune) bool {
 	_, ok := s.data[val]
 	return ok
 }
