be59414f6d8d86af49097e1f502520bd8ba0c655
wehrwein1 <23436190+wehrwein1@users.noreply.github.com>
2022-12-05 20:55:49 -0500

    add Size()

--- util/ds/stacks.go
+++ util/ds/stacks.go
@@ -17,11 +17,18 @@ func (s *Stack[T]) Peek() T {
 }
 
 func (s *Stack[T]) Pop() (val T) {
+	if s.IsEmpty() {
+		panic("cannot pop an empty stack")
+	}
 	val = s.data[len(s.data)-1]
 	s.data = s.data[:len(s.data)-1] // Pop
 	return
 }
 
 func (s *Stack[T]) IsEmpty() bool {
-	return len(s.data) == 0
+	return s.Size() == 0
+}
+
+func (s *Stack[T]) Size() int {
+	return len(s.data)
 }
