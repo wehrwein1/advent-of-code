507c9d160607bbddaded891def6e4312cc71d24e
wehrwein1 <23436190+wehrwein1@users.noreply.github.com>
2024-12-02 09:42:22 -0500

    2024 day 02

--- 2024/day02/day02.py
+++ 2024/day02/day02.py
@@ -2,35 +2,43 @@ from typing import List
 
 
 class Solution:
+
+    def check_safe(self, deltas: List[int]) -> bool:
+        in_value_range = lambda n: 1 <= abs(n) and abs(n) <= 3
+        is_positive = lambda n: n > 0
+        is_negative = lambda n: n < 0
+        # fmt:off
+        return all(map(in_value_range, deltas)) and \
+                (all(map(is_positive, deltas)) or \
+                 all(map(is_negative, deltas)))
+        # fmt:on
+
+    def make_deltas(self, values: List[int]) -> List[int]:
+        deltas = []
+        for i in range(1, len(values)):
+            pair = values[i - 1 : i + 1]
+            delta = pair[0] - pair[1]
+            deltas.append(delta)
+        return deltas
+
     def computeDay(self, lines: List[str], part: int):
         safe_count = 0
         for line in lines:
-            deltas = []
             values = list(map(int, line.split()))
-            for i in range(1, len(values)):
-                pair = values[i - 1 : i + 1]
-                delta = pair[0] - pair[1]
-                deltas.append(delta)
-
-            in_value_range = lambda n: 1 <= abs(n) and abs(n) <= 3
-            is_positive = lambda n: n > 0
-            is_negative = lambda n: n < 0
             if part == 1:
-                # fmt:off
-                safe = all(map(in_value_range, deltas)) and \
-                       (all(map(is_positive, deltas)) or \
-                        all(map(is_negative, deltas)))
-                # fmt:on
-            else:
-                # fmt:off
-                n = len(deltas)
-                allowed = [n-1, n]
-                safe = len(list(map(in_value_range, deltas))) in allowed and \
-                       (len(list(map(is_positive, deltas))) in allowed) or \
-                        len(list(map(is_negative, deltas))) in allowed
-                # fmt:on
-            print(f"line: [{line}], safe: {safe}")
-            if safe:
+                deltas = self.make_deltas(values)
+                is_safe = self.check_safe(deltas)
+            elif part == 2:
+                n = len(values)
+                for i in range(n):
+                    values_ = values.copy()
+                    del values_[i]
+                    deltas = self.make_deltas(values_)
+                    is_safe = self.check_safe(deltas)
+                    if is_safe:
+                        break
+            print(f"line: [{line}], safe: {is_safe}")
+            if is_safe:
                 safe_count += 1
         return safe_count
 
