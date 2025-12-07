fbad13c194c73ec55accd311eccaa040c1112924
wehrwein1 <23436190+wehrwein1@users.noreply.github.com>
2025-12-07 09:35:07 -0500

    Refactor to decouple from INPUT.txt files

--- 2020/day05.py
+++ 2020/day05.py
@@ -1,7 +1,6 @@
 # https://adventofcode.com/2020/day/5
+from typing import List
 from pyutil.fileio import file_lines
-
-lines = file_lines('2020/input/05_INPUT.txt')
 
 def compute_row(row_spec : str) -> int:
   return int(''.join(list(map(lambda char : '1' if char =='B' else '0', list(row_spec)))), 2)
@@ -16,7 +15,17 @@ def compute_seat_id(seat_spec):
   print(" spec={}, row={}, col={}, seat_id={}".format(seat_spec, row, col, seat_id))
   return seat_id
 
-seat_ids = [compute_seat_id(line) for line in lines]
-min_seat, max_seat = min(seat_ids), max(seat_ids)
-empty_seats = [seat_id for seat_id in range(min_seat, max_seat) if not seat_id in seat_ids]
-print("min_seat={}, max_seat={}, empty_seats={}".format(min_seat, max_seat, empty_seats))
+def computeDay(self, lines: List[str], part: int) -> int:
+    seat_ids = [compute_seat_id(line) for line in lines]
+    min_seat, max_seat = min(seat_ids), max(seat_ids)
+    empty_seats = [seat_id for seat_id in range(min_seat, max_seat) if not seat_id in seat_ids]
+    print("min_seat={}, max_seat={}, empty_seats={}".format(min_seat, max_seat, empty_seats))
+
+# bootstrap
+def main():
+    print(f"part 1: {computeDay(file_lines('2020/input/05_INPUT.txt'), 1)}")
+    print(f"part 2: {computeDay(file_lines('2020/input/05_INPUT.txt'), 2)}")
+
+
+if __name__ == "__main__":
+    main()
